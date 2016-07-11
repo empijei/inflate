ac2c664bcaee3670288c9e7e80439aacfbd27dad
RobClap <robclap8@gmail.com>
2016-07-11 11:48:32 +0200

    Now can support different algorithms.

--- inflate.go
+++ inflate.go
@@ -3,6 +3,7 @@ package main
 import (
 	"compress/flate"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -10,11 +11,12 @@ import (
 
 var d = flag.Bool("d", false, "Deflates input instead")
 var l = flag.Int("l", -1, "Deflate compression level")
+var alg = flag.String("alg", "flate", "The compression algorithm to use")
 
 func main() {
 	flag.Parse()
 	if *d {
-		w, err := flate.NewWriter(os.Stdout, *l)
+		w, err := NewWriter()
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -27,8 +29,11 @@ func main() {
 			log.Fatal(err)
 		}
 	} else {
-		r := flate.NewReader(os.Stdin)
-		_, err := io.Copy(os.Stdout, r)
+		r, err := NewReader()
+		if err != nil {
+			log.Fatal(err)
+		}
+		_, err = io.Copy(os.Stdout, r)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -36,5 +41,23 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+	}
+}
+
+func NewWriter() (io.WriteCloser, error) {
+	switch *alg {
+	case "flate":
+		return flate.NewWriter(os.Stdout, *l)
+	default:
+		return nil, fmt.Errorf("Unknown Algorithm")
+	}
+}
+
+func NewReader() (io.ReadCloser, error) {
+	switch *alg {
+	case "flate":
+		return flate.NewReader(os.Stdin), nil
+	default:
+		return nil, fmt.Errorf("Unknown Algorithm")
 	}
 }
