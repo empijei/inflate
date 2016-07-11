68c25799d615af31de53d996ee6e0c792111feb7
RobClap <robclap8@gmail.com>
2016-07-11 11:57:57 +0200

    Added zlib and help

--- inflate.go
+++ inflate.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"compress/flate"
+	"compress/zlib"
 	"flag"
 	"fmt"
 	"io"
@@ -11,43 +12,39 @@ import (
 
 var d = flag.Bool("d", false, "Deflates input instead")
 var l = flag.Int("l", -1, "Deflate compression level")
-var alg = flag.String("alg", "flate", "The compression algorithm to use")
+var alg = flag.String("alg", "flate", "The compression algorithm to use.\n\tSupported algorithms:\n\t\tflate\n\t\tzlib\n")
 
 func main() {
 	flag.Parse()
 	if *d {
 		w, err := NewWriter()
-		if err != nil {
-			log.Fatal(err)
-		}
+		errFatal(err)
 		_, err = io.Copy(w, os.Stdin)
-		if err != nil {
-			log.Fatal(err)
-		}
+		errFatal(err)
 		err = w.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
+		errFatal(err)
 	} else {
 		r, err := NewReader()
-		if err != nil {
-			log.Fatal(err)
-		}
+		errFatal(err)
 		_, err = io.Copy(os.Stdout, r)
-		if err != nil {
-			log.Fatal(err)
-		}
+		errFatal(err)
 		err = r.Close()
-		if err != nil {
-			log.Fatal(err)
-		}
+		errFatal(err)
 	}
 }
 
+/*TODO
+https://golang.org/pkg/compress/gzip/
+https://golang.org/pkg/compress/lzw/
+https://golang.org/pkg/compress/bzip2/
+*/
+
 func NewWriter() (io.WriteCloser, error) {
 	switch *alg {
-	case "flate":
+	case "flate", "f":
 		return flate.NewWriter(os.Stdout, *l)
+	case "zlib", "z":
+		return zlib.NewWriter(os.Stdout), nil
 	default:
 		return nil, fmt.Errorf("Unknown Algorithm")
 	}
@@ -55,9 +52,17 @@ func NewWriter() (io.WriteCloser, error) {
 
 func NewReader() (io.ReadCloser, error) {
 	switch *alg {
-	case "flate":
+	case "flate", "f":
 		return flate.NewReader(os.Stdin), nil
+	case "zlib", "z":
+		return zlib.NewReader(os.Stdin)
 	default:
 		return nil, fmt.Errorf("Unknown Algorithm")
+	}
+}
+
+func errFatal(err error) {
+	if err != nil {
+		log.Fatal(err)
 	}
 }
